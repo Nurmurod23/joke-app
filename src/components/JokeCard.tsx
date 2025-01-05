ceacf1c8b139610034de34dae6a388527fd47ee1
Nurmurod23 <elow9020@gmail.com>
2025-01-05 14:26:21 +0500

    aafn

--- src/components/JokeCard.tsx
+++ src/components/JokeCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { ThumbsUp } from 'lucide-react';
 import { supabase } from '../lib/supabase';
 import toast from 'react-hot-toast';
